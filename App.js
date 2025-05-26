a2eb5ecf947a633890bba720af79af9fd4cac30c
Mohit Kucheriya <kucheriyamohit@gmail.com>
2025-05-26 11:26:37 +0530

    Episode-01_The Inception

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "Getting Statrted with React"
+);
+
+const nestedEl = React.createElement("div", { id: "parent" }, [
+    React.createElement("h1", { id: "sibling1" }, "I am h1 tag"),
+    React.createElement("h2", { id: "sibling2" }, "I am h2 tag"),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(nestedEl);
